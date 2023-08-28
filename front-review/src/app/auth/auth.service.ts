005a8da710e654c27aea1fa518e3f44374cedff7
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-29 03:07:56 +0500

    fixed env

--- front-review/src/app/auth/auth.service.ts
+++ front-review/src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment'
+import { environment } from 'src/environments/environment'
 import { AuthRequest, AuthResponse } from './auth.model';
 import { Observable, catchError, map, of, throwError } from 'rxjs';
 
