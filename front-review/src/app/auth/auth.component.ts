50aa03c89e17c3ed7f0356a87065eb2a770b713c
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-28 10:58:11 +0500

    working pure google auth

--- front-review/src/app/auth/auth.component.ts
+++ front-review/src/app/auth/auth.component.ts
@@ -8,61 +8,52 @@ import {
   SocialAuthService,
   GoogleLoginProvider,
 } from '@abacritt/angularx-social-login';
+import { HttpClient } from '@angular/common/http';
 
-declare const google: any;
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-  loginForm!: FormGroup;
-  socialUser!: SocialUser;
-  isLoggedin?: boolean;
+  private accessToken = '';
+  user: SocialUser;
+  loggedIn: boolean;
 
   constructor(
-    private formBuilder: FormBuilder,
-    private socialAuthService: SocialAuthService
+    private authService: SocialAuthService,
+    private httpClient: HttpClient
   ) {}
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-    this.socialAuthService.authState.subscribe((user) => {
-      this.socialUser = user;
-      this.isLoggedin = user != null;
-      console.log(this.socialUser);
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
     });
   }
 
-  ngAfterViewInit() {
-    this.initializeGoogleSignIn();
+  // Request google Access Token
+  getAccessToken(): void {
+    this.authService
+      .getAccessToken(GoogleLoginProvider.PROVIDER_ID)
+      .then((accessToken) => (this.accessToken = accessToken));
   }
 
-  private initializeGoogleSignIn(): void {
-    google.accounts.id.initialize({
-      client_id: '478306182791-gp3oe0veh3j5042f2licqcd7eflljdt6.apps.googleusercontent.com',
-      callback: this.handleCredentialResponse,
-    });
+  getGoogleCalendarData(): void {
+    if (!this.accessToken) return;
 
-    google.accounts.id.renderButton(
-      document.getElementById('googleSignInButtonContainer'), // Use the correct ID
-      { theme: 'outline', size: 'large' }
-    );
+    this.httpClient
+      .get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        headers: { Authorization: `Bearer ${this.accessToken}` },
+      })
+      .subscribe((events) => {
+        alert('Look at your console');
+        console.log('events', events);
+      });
+  }
+  // Refresh google Access Token
+  refreshToken(): void {
+    this.authService.refreshAccessToken(GoogleLoginProvider.PROVIDER_ID);
   }
 
-  private handleCredentialResponse(response: any): void {
-    console.log('Encoded JWT ID token: ' + response.credential);
-    // Handle the JWT response as needed
-  }
-
-  loginWithGoogle(): void {
-    google.accounts.id.prompt(); // Trigger the manual Google Sign-In process
-  }
-
-  logOut(): void {
-    this.socialAuthService.signOut();
-  }
 }
