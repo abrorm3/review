68682b9ecbcb747194ab6226cadccfeee681228b
abrorm3 <abrormukhammadiev@gmail.com>
2023-09-15 03:32:23 +0500

    custom Angular editor image upload

--- front-review/src/app/create-review/create-review.component.ts
+++ front-review/src/app/create-review/create-review.component.ts
@@ -17,7 +17,10 @@ import {
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { MatChipInputEvent } from '@angular/material/chips';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
+import {
+  AngularEditorComponent,
+  AngularEditorConfig,
+} from '@kolkov/angular-editor';
 import { ImageUploadService } from '../shared/image-upload.service';
 import { AuthService } from '../auth/auth.service';
 import { editorConfig } from './editor-config';
@@ -30,6 +33,8 @@ export class CreateReviewComponent implements OnInit {
   @ViewChild('groupTypeAuto') groupTypeAuto: MatAutocompleteTrigger;
   @ViewChild('artAuto') artAuto: MatAutocompleteTrigger;
   @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('editor') editor: AngularEditorComponent;
+
   announcer = inject(LiveAnnouncer);
   userId = '';
   editorConfig = editorConfig;
@@ -38,6 +43,7 @@ export class CreateReviewComponent implements OnInit {
   isError: boolean = false;
   isSuccess: boolean = false;
   isLoading = false;
+  isImgLoading=false;
 
   filteredOptions: Observable<string[]>;
   filteredArts: Observable<string[]>;
@@ -161,6 +167,8 @@ export class CreateReviewComponent implements OnInit {
     const content = this.htmlContent.value;
     const authorRate = this.authorRateControl.value;
     const tags = this.tags;
+    console.log(content);
+
     if (!content || content.trim().length === 0) {
       this.isError = true;
       this.isSuccess = false;
@@ -191,8 +199,6 @@ export class CreateReviewComponent implements OnInit {
         },
       });
   }
-
-  onFileSelected(number) {}
 
   // tags
   add(event: MatChipInputEvent): void {
@@ -225,10 +231,31 @@ export class CreateReviewComponent implements OnInit {
     this.tagCtrl.setValue(null);
   }
 
-  uploadImage(file: File): Promise<string> {
-    return this.imageUploadService.uploadImage(
-      file,
-      `${this.userId}/${this.reviewTitleControl}`
-    );
+  onImageUpload() {
+    const inputElement = document.createElement('input');
+    inputElement.type = 'file';
+    inputElement.accept = 'image/*';
+
+    // Listen for file selection
+    inputElement.addEventListener('change', (event) => {
+      const file = (event.target as HTMLInputElement).files[0];
+
+      if (file) {
+        this.isImgLoading=true;
+        // Upload the image to Firebase Storage
+        this.imageUploadService
+          .uploadImage(file, `${this.userId}/${this.reviewTitleControl.value}`)
+          .then((downloadUrl) => {
+            // Inserting the image URL into the editor
+            const html = `<img src="${downloadUrl}" alt="Uploaded Image" />`;
+            this.isImgLoading=false;
+            // Using execCommand to insert HTML at the current cursor position, but yeah it is deprecated
+            document.execCommand('insertHTML', false, html);
+          });
+      }
+    });
+
+    // Triggering the file input dialog
+    inputElement.click();
   }
 }
