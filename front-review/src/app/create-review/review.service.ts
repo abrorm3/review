c52ff20fb3e670ccc0d2ab180dac21b8f9278653
abrorm3 <abrormukhammadiev@gmail.com>
2023-09-16 14:21:25 +0500

    changing backend to prod

--- front-review/src/app/create-review/review.service.ts
+++ front-review/src/app/create-review/review.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
